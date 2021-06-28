a1f831c3d42d372dc02e0b4d26daa4f61cbe3034
Matas Mikušauskas <45999356+mateusas96@users.noreply.github.com>
2021-06-28 14:28:19 +0300

    created posts CRUD, added post editing/creating form

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose';
+import express from 'express';
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -22,4 +24,42 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).send('No post with that id');
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+
+    res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const { id: _id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).send('No post with that id');
+    }
+
+    const deletedPost = await PostMessage.findByIdAndRemove(_id);
+
+    res.json(deletedPost);
+}
+
+export const likePost = async (req, res) => {
+    const { id: _id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).send('No post with that id');
+    }
+
+    const post = await PostMessage.findById(_id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { likeCount: post.likeCount + 1 }, { new: true });
+
+    res.json(updatedPost);
 }
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
-app.use('/posts', postRoutes);
+dotenv.config();
 
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://mern-app-master:Mern-App-Master232@mern-app.jm70m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const MONGO_DB_CONNECTION_URL = process.env.MONGO_DB_CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_DB_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
+
+app.use('/posts', postRoutes);
